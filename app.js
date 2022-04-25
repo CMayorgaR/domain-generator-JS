cacca75ae00e2260fe63b6aa8a240a8987d7a0ea
CMayorgaR <c.pmayorgar@gmail.com>
2022-04-25 21:46:18 +0000

    hola

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let noun = ['jogger','racoon'];
 for (let i=0; i<pronoun.length; i++){
     for (let j=0; j<adj.length; j++)  {
         for (let k=0; k<noun.length; k++) {
-            console.log (pronoun[i]+adj[j]+noun[k]+".com")
+            console.log ("www."+pronoun[i]+adj[j]+noun[k]+".com")
         }
     }
 }
